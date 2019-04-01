ab80001d14fab0feac56eefed9b67a3b003defc4
Neuron <neuron@rados.io>
2019-04-01 12:17:09 +0100

    remove dist

--- dist/utils/index.d.ts
+++
@@ -1,4 +0,0 @@
-export declare function sleep(ms: number): Promise<{}>;
-export declare const etherAddress: "0x0000000000000000000000000000000000000000";
-export declare const etherDecimals: 18;
-export declare const gaslimit: 400000;
--- dist/utils/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-function sleep(ms) {
-    return new Promise(resolve => setTimeout(resolve, ms));
-}
-exports.sleep = sleep;
-exports.etherAddress = '0x0000000000000000000000000000000000000000';
-exports.etherDecimals = 18;
-exports.gaslimit = 400000;
